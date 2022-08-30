62d916c7c7740efae52f29fde1093cdfdc5ccded
LakshyaTecno <LakshyaTecno@gmail.com>
2022-08-30 12:16:10 +0530

    filtering

--- controllers/course.controller.js
+++ controllers/course.controller.js
@@ -73,69 +73,23 @@ exports.getAllCourse = async (req, res) => {
   const priceMin = req.query.min;
   const priceMax = req.query.max;
   try {
+    let queryObj = {};
     const courses = await Course.find();
-
-    let courseMatchesWithName = [];
+    let filetred_data = [];
     if (courseName) {
-      courseMatchesWithName = await Course.find({
+      filetred_data = await Course.find({
         name: { $regex: new RegExp(courseName) },
       });
+    } else {
+      queryObj["$or"] = [
+        { name: courseName },
+        { category: courseCategory },
+        { partner: coursePartner },
+        { rating: courseRating },
+        { price: { $gte: priceMin, $lte: priceMax } },
+      ];
+      filetred_data = await Course.find(queryObj);
     }
-
-    var courseMatchesWithRating = [];
-    if (courseRating) {
-      courseMatchesWithRating = await Course.find({
-        rating: courseRating,
-      });
-    }
-
-    var courseMatchesWithCategory = [];
-    if (courseCategory) {
-      courseMatchesWithCategory = await Course.find({
-        category: courseCategory,
-      });
-    }
-
-    var courseMatchesWithPartner = [];
-    if (coursePartner) {
-      courseMatchesWithPartner = await Course.find({ partner: coursePartner });
-    }
-
-    var courseMatchesWithPrice = [];
-    if (priceMin || priceMax) {
-      courseMatchesWithPrice = await Course.find({
-        price: { $gte: priceMin, $lte: priceMax },
-      });
-    }
-    let name = [];
-    let category = [];
-    let partner = [];
-    let price = [];
-    let rating = [];
-    courseMatchesWithName.map((obj) => {
-      name.push(obj.name);
-    });
-    courseMatchesWithCategory.map((obj) => {
-      category.push(obj.category);
-    });
-    courseMatchesWithPrice.map((obj) => {
-      price.push(obj.price);
-    });
-    courseMatchesWithPartner.map((obj) => {
-      partner.push(obj.partner);
-    });
-    courseMatchesWithRating.map((obj) => {
-      rating.push(obj.rating);
-    });
-    queryObj["$and"] = [
-      { name: { $in: name } },
-      { category: { $in: category } },
-      { partner: { $in: partner } },
-      { rating: { $in: rating } },
-      { price: { $in: price } },
-    ];
-
-    let filetred_data = await Course.find(queryObj);
 
     console.log(filetred_data);
     filetred_data = filetred_data.filter(
